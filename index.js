cb4cf534ea87fc8dda81a5fbcf7e0eed195236e6
Ryan Weaver <ryan@thatsquality.com>
2017-03-05 14:48:45 -0500

    Updating how we expose the public API

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
-var Remix = require('./lib/Remix');
+const Remix = require('./lib/Remix');
+const config_generator = require('./lib/config_generator');
 
-module.exports = new Remix();
+remix = new Remix();
+
+module.exports = {
+    setOutputPath(outputPath) {
+        remix.setOutputPath(outputPath);
+
+        return this;
+    },
+
+    setPublicPath(publicPath) {
+        remix.setPublicPath(publicPath);
+
+        return this;
+    },
+
+    setPublicCDNPath(publicCDNPath) {
+        remix.setPublicCDNPath(publicCDNPath);
+
+        return this;
+    },
+
+    addEntry(name, src) {
+        remix.addEntry(name, src);
+
+        return this;
+    },
+
+    addStylesEntry(name, src) {
+        remix.addStylesEntry(name, src);
+
+        return this;
+    },
+
+    enableVersioning(enabled = true) {
+        remix.enableVersioning(enabled);
+
+        return this;
+    },
+
+    enableSourceMaps(enabled = true) {
+        remix.enableSourceMaps(enabled);
+
+        return this;
+    },
+
+    extractVendorEntry(name, files) {
+        remix.extractVendorEntry(name, files);
+
+        return this;
+    },
+
+    autoProvideVariables(variables) {
+        remix.autoProvideVariables(variables);
+
+        return this;
+    },
+
+    autoProvidejQuery() {
+        remix.autoProvidejQuery();
+
+        return this;
+    },
+
+    isProduction() {
+        return remix.isProduction();
+    },
+
+    getWebpackConfig() {
+        return config_generator(remix);
+    }
+};
