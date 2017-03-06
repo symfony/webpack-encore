33e7cabd4c2c99f53c5dee1e4a54bfb4466a21c4
Ryan Weaver <ryan@thatsquality.com>
2017-03-05 21:09:05 -0500

    Adding more tests

--- test/WebpackConfig.js
+++ test/WebpackConfig.js
@@ -1,14 +1,57 @@
 var expect    = require('chai').expect;
 var WebpackConfig = require('../lib/WebpackConfig');
+var path = require('path');
+var fs = require('fs');
 
 describe('WebpackConfig object', () => {
 
-    describe('setOutput', () => {
+    describe('setOutputPath', () => {
         it('use absolute, existent path', () => {
             var config = new WebpackConfig();
             config.setOutputPath(__dirname);
 
             expect(config.outputPath).to.equal(__dirname);
+        });
+
+        it('relative path, becomes absolute', () => {
+            var config = new WebpackConfig();
+            config.setOutputPath('assets');
+
+            // the "context" dir is resolved to the directory
+            // of this plugin, because it holds a package.json
+            expect(config.outputPath).to.equal(
+                path.join(__dirname, '../assets')
+            );
+
+            // cleanup!
+            fs.rmdirSync(path.join(__dirname, '../assets'));
+        });
+
+        it('non-existent path creates directory', () => {
+            var targetPath = path.join(__dirname, 'new_dir');
+            if (fs.existsSync(targetPath)) {
+                fs.rmdirSync(targetPath);
+            }
+
+            var config = new WebpackConfig();
+            config.setOutputPath(targetPath);
+
+            expect(fs.existsSync(config.outputPath)).to.be.true;
+
+            // cleanup!
+            fs.rmdirSync(targetPath);
+        });
+
+        it('non-existent directory, 2 levels deep throws error', () => {
+            var targetPath = path.join(__dirname, 'new_dir', 'subdir');
+            if (fs.existsSync(targetPath)) {
+                fs.rmdirSync(targetPath);
+            }
+
+            var config = new WebpackConfig();
+            expect(() => {
+                config.setOutputPath(targetPath);
+            }).to.throw('create this directory');
         });
     });
 });
