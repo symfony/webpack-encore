7e5e0fc5d69b076c9d3f6ca1c5b8b3f6aff2513d
Ryan Weaver <ryan@thatsquality.com>
2017-03-12 14:51:33 -0400

    Lots of test refactoring and additions
    Especially to make sure that we have the publicPath correct

--- test/functional.js
+++ test/functional.js
@@ -18,34 +18,38 @@ Browser.extend(function (browser) {
 const testProjectPath = path.join(__dirname, 'project');
 
 /**
- * @param contextDirName
+ * @param outputDirName Sub-directory to output stuff
  * @returns {WebpackConfig}
  */
-function createWebpackConfig(contextDirName) {
-    var config = new WebpackConfig(path.join(__dirname, 'fixtures', contextDirName));
+function createWebpackConfig(outputDirName = '') {
+    var config = new WebpackConfig(path.join(__dirname, 'fixtures'));
 
-    if (!fs.existsSync(testOutputPath)) {
-        fs.mkdirSync(testOutputPath);
+    if (!fs.existsSync(testProjectPath)) {
+        fs.mkdirSync(testProjectPath);
     }
-    config.setOutputPath(testOutputPath);
+
+    var outputPath = path.join(testProjectPath, outputDirName);
+    // allows us to create a few levels deep without issues
+    fs.mkdirsSync(outputPath);
+    config.setOutputPath(path.join(testProjectPath, outputDirName));
 
     return config;
 }
 
 function emptyTestDir() {
-    fs.emptyDirSync(testOutputPath);
+    fs.emptyDirSync(testProjectPath);
 }
 
 function assertOutputFileContains(filepath, expectedContents) {
-    var fullPath = path.join(testOutputPath, filepath);
+    var fullPath = path.join(testProjectPath, filepath);
     const actualContents = fs.readFileSync(fullPath, 'utf8');
     if (!actualContents.includes(expectedContents)) {
         throw new Error(`Expected contents "${expectedContents}" not found in file ${fullPath}`);
     }
 }
 
-function assertManifestPath(sourcePath, expectedDestinationPath) {
-    const manifestData = loadManifest();
+function assertManifestPath(outputDir, sourcePath, expectedDestinationPath) {
+    const manifestData = loadManifest(outputDir);
 
     if (!manifestData[sourcePath]) {
         throw new Error(`No ${sourcePath} key found in manifest ${manifestData}`);
@@ -55,18 +59,18 @@ function assertManifestPath(sourcePath, expectedDestinationPath) {
         throw new Error(`source path ${sourcePath} expected to be set to ${expectedDestinationPath}, was actually ${manifestData[sourcePath]}`);
     }
 }
-function assertManifestPathDoesNotExist(sourcePath) {
-    const manifestData = loadManifest();
+function assertManifestPathDoesNotExist(outputDir, sourcePath) {
+    const manifestData = loadManifest(outputDir);
 
     if (manifestData[sourcePath]) {
         throw new Error(`Source ${sourcePath} key WAS found in manifest, but should not be there!`);
     }
 }
 
-function loadManifest()
+function loadManifest(outputDir)
 {
     return JSON.parse(
-        fs.readFileSync(path.join(testOutputPath, 'manifest.json'), 'utf8')
+        fs.readFileSync(path.join(outputDir, 'manifest.json'), 'utf8')
     );
 }
 
@@ -105,9 +109,9 @@ describe('Functional tests using webpack', () => {
         });
 
         it('Builds a simple .js file + manifest.json', (done) => {
-            var config = createWebpackConfig('basic');
+            var config = createWebpackConfig('web/build');
             config.addEntry('main', './no_require');
-            config.setPublicPath('/public');
+            config.setPublicPath('/build');
 
             runWebpack(config, () => {
                 // should have a main.js file
@@ -118,17 +122,18 @@ describe('Functional tests using webpack', () => {
 
                 // check that main.js has the correct contents
                 assertOutputFileContains(
-                    'main.js',
+                    'web/build/main.js',
                     'no_require_loaded'
                 );
                 // check that main.js has the webpack bootstrap
                 assertOutputFileContains(
-                    'main.js',
+                    'web/build/main.js',
                     '__webpack_require__'
                 );
                 assertManifestPath(
-                    'public/main.js',
-                    'public/main.js'
+                    config.outputPath,
+                    'build/main.js',
+                    'build/main.js'
                 );
 
                 done();
@@ -136,10 +141,12 @@ describe('Functional tests using webpack', () => {
         });
 
         it('setPublicCDNPath causes paths to use CDN', (done) => {
-            var config = createWebpackConfig('basic');
+            var config = createWebpackConfig('public/assets');
             config.addEntry('main', './code_splitting');
-            config.setPublicPath('/public');
-            config.setPublicCDNPath('http://localhost:8090');
+            config.setPublicPath('/assets');
+            config.setPublicCDNPath('http://localhost:8090/assets');
+
+            // todo - test paths to images/fonts inside CSS file
 
             runWebpack(config, () => {
                 expect(config.outputPath).to.be.a.directory()
@@ -147,18 +154,19 @@ describe('Functional tests using webpack', () => {
 
                 // check that the publicPath is set correctly
                 assertOutputFileContains(
-                    'main.js',
-                    '__webpack_require__.p = "http://localhost:8090/";'
+                    'public/assets/main.js',
+                    '__webpack_require__.p = "http://localhost:8090/assets/";'
                 );
 
                 /*
                  * An experimental thing... where we actually use a browser to try things...
                  *
-                 * To get this to pass, you must start 2 servers in the test/public directory
+                 * To get this to pass, you must start 2 servers in a test/project/public directory
                  *      http-server
                  *      http-server -p 8090
                  */
-                fs.copySync(path.join(__dirname, 'testing.html'), path.join(testOutputPath, 'testing.html'));
+                // copy to the public root
+                fs.copySync(path.join(__dirname, 'testing.html'), path.join(testProjectPath, 'public', 'testing.html'));
                 var browser = new Browser();
                 browser.visit('http://127.0.0.1:8080/testing.html', () => {
                     // check that both JS files were loaded!
@@ -170,9 +178,9 @@ describe('Functional tests using webpack', () => {
         });
 
         it('addStyleEntry .js files are removed', (done) => {
-            var config = createWebpackConfig('basic');
+            var config = createWebpackConfig('web');
             config.addEntry('main', './no_require');
-            config.setPublicPath('/public');
+            config.setPublicPath('/');
             config.addStyleEntry('styles', './h1_style.css');
 
             runWebpack(config, () => {
@@ -181,7 +189,7 @@ describe('Functional tests using webpack', () => {
                     .with.files(['main.js', 'styles.css', 'manifest.json']);
 
                 assertOutputFileContains(
-                    'styles.css',
+                    'web/styles.css',
                     'font-size: 50px;'
                 );
                 // todo - this isn't the biggest deal, but it's failing
@@ -194,9 +202,9 @@ describe('Functional tests using webpack', () => {
         });
 
         it('enableVersioning applies to js, css & manifest', (done) => {
-            var config = createWebpackConfig('basic');
+            var config = createWebpackConfig('web/build');
             config.addEntry('main', './no_require');
-            config.setPublicPath('/public');
+            config.setPublicPath('/build');
             config.addStyleEntry('h1', './h1_style.css');
             config.addStyleEntry('bg', './background_image.scss');
             config.enableVersioning(true);
@@ -204,9 +212,9 @@ describe('Functional tests using webpack', () => {
             runWebpack(config, () => {
                 expect(config.outputPath).to.be.a.directory()
                     .with.files([
-                        'main.43bfd7b29d2e414807da.js',
+                        'main.dba523dfb21e7b8de3e9.js',
                         'h1.c84caea6dd12bba7955dee9fedd5fd03.css',
-                        'bg.bf0ae369481a1080946f3112ee958040.css',
+                        'bg.4110c672dd434294dc4e28d1475255e0.css',
                         'manifest.json'
                     ]
                 );
@@ -218,8 +226,53 @@ describe('Functional tests using webpack', () => {
                 );
 
                 assertOutputFileContains(
-                    'bg.bf0ae369481a1080946f3112ee958040.css',
-                    '/images/symfony_logo.ea1ca6f7f3719118f301a5cfcb1df3c0.png'
+                    'web/build/bg.4110c672dd434294dc4e28d1475255e0.css',
+                    '/build/images/symfony_logo.ea1ca6f7f3719118f301a5cfcb1df3c0.png'
+                );
+
+                done();
+            });
+        });
+
+        it('font and image files are copied correctly', (done) => {
+            var config = createWebpackConfig('www/build');
+            config.setPublicPath('/build');
+            config.addStyleEntry('bg', './background_image.scss');
+            config.addStyleEntry('font', './roboto_font.css');
+
+            runWebpack(config, () => {
+                expect(config.outputPath).to.be.a.directory()
+                    .with.files([
+                        'bg.css',
+                        'font.css',
+                        'manifest.json'
+                    ]
+                );
+
+                expect(path.join(config.outputPath, 'images')).to.be.a.directory()
+                    .with.files([
+                        'symfony_logo.png'
+                    ]
+                );
+
+                expect(path.join(config.outputPath, 'fonts')).to.be.a.directory()
+                    .with.files([
+                        'Roboto.woff2'
+                    ]
+                );
+
+                assertOutputFileContains(
+                    'www/build/bg.css',
+                    '/build/images/symfony_logo.png'
+                );
+                assertOutputFileContains(
+                    'www/build/font.css',
+                    '/build/fonts/Roboto.woff2'
+                );
+
+                assertOutputFileContains(
+                    'www/build/font.css',
+                    '/build/fonts/Roboto.woff2'
                 );
 
                 done();
