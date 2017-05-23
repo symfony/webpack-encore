f1dcd9b37783a03b86cd83355941aa7fccd57ee1
Ryan Weaver <ryan@thatsquality.com>
2017-05-23 09:07:06 -0400

    Using webpack and webpack-dev-server internal packages

--- bin/encore.js
+++ bin/encore.js
@@ -46,31 +46,13 @@ switch (command) {
 }
 
 if (commandConfig.useDevServer) {
-    const binPath = findExecutable('webpack-dev-server');
-    if (null === binPath) {
-        console.log(chalk.bgRed.white(`Install webpack-dev-server to use the dev-server option`));
-        console.log();
-        console.log(`    ${chalk.green('yarn install webpack-dev-server --dev')}`)
-        console.log();
-
-        process.exit(1);
-    }
-
     console.log('Running webpack-dev-server ...');
-    return require(binPath);
+
+    return require('webpack-dev-server/bin/webpack-dev-server');
 } else {
-    const binPath = findExecutable('webpack');
-    if (null === binPath) {
-        console.log(chalk.bgRed.white(`Install webpack before running this command`));
-        console.log();
-        console.log(`    ${chalk.green('yarn install webpack --dev')}`)
-        console.log();
-
-        process.exit(1);
-    }
-
     console.log('Running webpack ...');
-    return require(binPath);
+
+    return require('webpack/bin/webpack');
 }
 
 function showUsageInstructions() {
@@ -92,20 +74,3 @@ function showUsageInstructions() {
     console.log();
 }
 
-function findExecutable(binName) {
-    const packagesJsonPath = pkgUp.sync(process.cwd());
-    if (null === packagesJsonPath) {
-        console.log(chalk.bgRed.white(`Cannot find the webpack executable (no ${chalk.bold('package.json')} found). Are you executing webpack from a directory outside of your project?`));
-
-        process.exit(1);
-    }
-
-    const binPath = path.join(path.dirname(packagesJsonPath), 'node_modules', '.bin', binName);
-    try {
-        require.resolve(binPath);
-    } catch (e) {
-        return;
-    }
-
-    return binPath;
-}
