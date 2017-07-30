d2cc270a9c9c2f043119220fff4df39f6a96c2db
David Paz <davidmpaz@gmail.com>
2017-07-30 12:20:32 +0200

    Fix linting indentation

--- test/functional.js
+++ test/functional.js
@@ -288,13 +288,13 @@ describe('Functional tests using webpack', function() {
                         'bg.483832e48e67e6a3b7f0ae064eadca51.css',
                         'manifest.json'
                     ]
-                );
+                    );
 
                 expect(path.join(config.outputPath, 'images')).to.be.a.directory()
                     .with.files([
                         'symfony_logo.ea1ca6f7.png'
                     ]
-                );
+                    );
 
                 webpackAssert.assertOutputFileContains(
                     'bg.483832e48e67e6a3b7f0ae064eadca51.css',
@@ -319,19 +319,19 @@ describe('Functional tests using webpack', function() {
                         'font.css',
                         'manifest.json'
                     ]
-                );
+                    );
 
                 expect(path.join(config.outputPath, 'images')).to.be.a.directory()
                     .with.files([
                         'symfony_logo.ea1ca6f7.png'
                     ]
-                );
+                    );
 
                 expect(path.join(config.outputPath, 'fonts')).to.be.a.directory()
                     .with.files([
                         'Roboto.9896f773.woff2'
                     ]
-                );
+                    );
 
                 webpackAssert.assertOutputFileContains(
                     'bg.css',
@@ -359,21 +359,21 @@ describe('Functional tests using webpack', function() {
                         'styles.css',
                         'manifest.json'
                     ]
-                );
+                    );
 
                 expect(path.join(config.outputPath, 'images')).to.be.a.directory()
                     .with.files([
                         'symfony_logo.ea1ca6f7.png',
                         'symfony_logo.f27119c2.png'
                     ]
-                );
+                    );
 
                 expect(path.join(config.outputPath, 'fonts')).to.be.a.directory()
                     .with.files([
                         'Roboto.9896f773.woff2',
                         'Roboto.3c37aa69.woff2'
                     ]
-                );
+                    );
 
                 webpackAssert.assertOutputFileContains(
                     'styles.css',
@@ -587,7 +587,7 @@ module.exports = {
 
             fs.writeFileSync(
                 path.join(appDir, '.babelrc'),
-`
+                `
 {
   "presets": [
     ["env", {
