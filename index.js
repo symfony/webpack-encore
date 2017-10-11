adda17c1af52e1ee240e4372a4d6184fe0151b98
Nicolas <nikrou77@gmail.com>
2017-10-11 13:35:01 +0200

    Fix issue incorrect paths for fonts. Add methods to Use relative paths

--- index.js
+++ index.js
@@ -75,6 +75,18 @@ const publicApi = {
         return this;
     },
 
+    setFontsPublicPath(publicPath) {
+        webpackConfig.setFontsPublicPath(publicPath);
+
+        return this;
+    },
+
+    setImagesPublicPath(publicPath) {
+        webpackConfig.setImagesPublicPath(publicPath);
+
+        return this;
+    },
+
     /**
      * Used as a prefix to the *keys* in manifest.json. Not usually needed.
      *
