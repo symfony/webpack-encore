128ec007559236838464fca7c8eadf54f897bc37
Ryan Weaver <ryan@thatsquality.com>
2018-05-23 22:42:24 -0400

    Adding a warning of you accidentally override a core cache group

--- test/config/validator.js
+++ test/config/validator.js
@@ -82,4 +82,18 @@ describe('The validator function', () => {
         expect(logger.getMessages().warning).to.have.lengthOf(1);
         expect(logger.getMessages().warning[0]).to.include('Passing an absolute URL to setPublicPath() *and* using the dev-server can cause issues');
     });
+
+    it('warning with createSharedEntry() and core cache group name', () => {
+        const config = createConfig();
+        config.outputPath = '/tmp/public/build';
+        config.setPublicPath('/build');
+        config.createSharedEntry('vendors', './main');
+
+        logger.reset();
+        logger.quiet();
+        validator(config);
+
+        expect(logger.getMessages().warning).to.have.lengthOf(1);
+        expect(logger.getMessages().warning[0]).to.include('Passing "vendors" to createSharedEntry() is not recommended');
+    });
 });
