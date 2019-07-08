27cfe8eb4cfd5335d666a05ee546d10bc13c31a9
Arman <44655055+Arman-Hosseini@users.noreply.github.com>
2019-07-09 02:21:36 +0430

    Use HTTPS for links

--- fixtures/vuejs/components/Hello.vue
+++ fixtures/vuejs/components/Hello.vue
@@ -8,13 +8,13 @@
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="https://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="https://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
   </div>
