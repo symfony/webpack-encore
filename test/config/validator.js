7e32b0b07192655533f582fe1fe380567e91e1c2
Lyrkan <850046+Lyrkan@users.noreply.github.com>
2020-03-27 15:16:37 +0100

    Add a warning when calling addCacheGroup() with 'vendors' or the same than in createSharedEntry()

--- test/config/validator.js
+++ test/config/validator.js
@@ -113,4 +113,22 @@ describe('The validator function', () => {
         expect(logger.getMessages().warning).to.have.lengthOf(1);
         expect(logger.getMessages().warning[0]).to.include('Passing "defaultVendors" to addCacheGroup() is not recommended');
     });
+
+    it('warning with addCacheGroup() and a similar createSharedEntry() name', () => {
+        const config = createConfig();
+        config.outputPath = '/tmp/public/build';
+        config.setPublicPath('/build');
+        config.addEntry('main', './main');
+        config.createSharedEntry('foo', './foo.js');
+        config.addCacheGroup('foo', {
+            test: /[\\/]main/,
+        });
+
+        logger.reset();
+        logger.quiet();
+        validator(config);
+
+        expect(logger.getMessages().warning).to.have.lengthOf(1);
+        expect(logger.getMessages().warning[0]).to.include('Using the same name when calling createSharedEntry() and addCacheGroup() is not recommended.');
+    });
 });
