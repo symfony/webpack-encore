c5473a9a8aa76996a6130b9862143e783f240650
Christophe Coevoet <stof@notk.org>
2020-08-04 01:07:39 +0200

    Add more precise types for arguments forwarded to webpack
    webpack provides type definitions for their whole API, so we can
    reference their type rather than specifying a generic "object" type.
    Referencing the webpack types was already done for the returned
    configuration.

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ class Encore {
      * Encore.addPlugin(new MyWebpackPlugin(), PluginPriorities.DefinePlugin);
      * ```
      *
-     * @param {object} plugin
+     * @param {webpack.Plugin} plugin
      * @param {number} priority
      * @returns {Encore}
      */
@@ -309,7 +309,7 @@ class Encore {
     /**
      * Adds a custom loader config
      *
-     * @param {object} loader The loader config object
+     * @param {webpack.RuleSetRule} loader The loader config object
      *
      * @returns {Encore}
      */
@@ -322,7 +322,7 @@ class Encore {
     /**
      * Alias to addLoader
      *
-     * @param {object} rule
+     * @param {webpack.RuleSetRule} rule
      *
      * @returns {Encore}
      */
@@ -387,7 +387,7 @@ class Encore {
      * ]);
      * ```
      *
-     * @param {*} externals
+     * @param {webpack.ExternalsElement|webpack.ExternalsElement[]} externals
      *
      * @returns {Encore}
      */
@@ -902,7 +902,7 @@ class Encore {
      * ```
      *
      * Supported options:
-     *      * {Condition} exclude (default=/(node_modules|bower_components)/)
+     *      * {webpack.RuleSetCondition} exclude (default=/(node_modules|bower_components)/)
      *              A Webpack Condition passed to the JS/JSX rule that
      *              determines which files and folders should not be
      *              processed by Babel (https://webpack.js.org/configuration/module/#condition).
