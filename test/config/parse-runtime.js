9afe7594d8893a4f1ae4ed1a3e3d7a5f709ed07e
Atesca09 <atesca09@gmail.com>
2021-11-03 22:53:24 +0100

    Use clientWebSocketUrl instead of public paramter

--- test/config/parse-runtime.js
+++ test/config/parse-runtime.js
@@ -93,9 +93,9 @@ describe('parse-runtime', () => {
         expect(config.devServerHttps).to.equal(true);
     });
 
-    it('dev-server command public', () => {
+    it('dev-server command client-web-socket-url', () => {
         const testDir = createTestDirectory();
-        const config = parseArgv(createArgv(['dev-server', '--public', 'https://my-domain:8080']), testDir);
+        const config = parseArgv(createArgv(['dev-server', '--client-web-socket-url', 'https://my-domain:8080']), testDir);
 
         expect(config.devServerPublic).to.equal('https://my-domain:8080');
     });
