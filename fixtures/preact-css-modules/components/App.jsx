dbd5e5d37838716e42f1c41197ab7e16e41a9f39
Hugo Alliaume <hugo@alliau.me>
2024-09-20 12:28:00 +0200

    Add tests for CSS Modules with Preact

---
+++ fixtures/preact-css-modules/components/App.jsx
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+import './styles.css';
+import './styles.less';
+import './styles.scss';
+import './styles.stylus';
+import stylesCss from './styles.module.css?module';
+import stylesLess from './styles.module.less?module';
+import stylesScss from './styles.module.scss?module';
+import stylesStylus from './styles.module.stylus?module';
+
+export default function App() {
+    return <div className={`red large justified lowercase ${stylesCss.italic} ${stylesLess.underline} ${stylesScss.bold} ${stylesStylus.rtl}`}></div>
+}
